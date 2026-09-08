81eaa23d447cdec560080f996dd3ba095a85ea5b
thrish-martizano <martizanothrisha@gmail.com>
2026-09-08 13:51:06 +0800

    Added Insert

--- db/schema.sql
+++ db/schema.sql
@@ -34,4 +34,32 @@ CREATE TABLE IF NOT EXISTS users(
     user_update_at TIMESTAMP
         DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Insert Test Users
+INSERT INTO users
+(
+    user_email,
+    user_username,
+    user_password,
+    user_role
 )
+VALUES
+(
+    'admin@example.com',
+    'admin',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'admin'
+),
+(
+    'manager@example.com',
+    'manager',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'manager'
+),
+(
+    'user@example.com',
+    'user',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'user'
+);
